84005bbd7c77172e6878f741eb087913b8b2f5f6
payette28 <133899413+payette28@users.noreply.github.com>
2023-08-14 21:01:48 -0400

    Update models.js
    if applied implements CORS, password hashing and data validation

--- models.js
+++ models.js
@@ -1,28 +1,37 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 let movieSchema = mongoose.Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    genre: {
-      name: String,
-      description: String
-    },
-    director: {
-      name: String,
-      bio: String
-    }
-  });
-  
-  let userSchema = mongoose.Schema({
-    username: {type: String, required: true},
-    password: {type: String, required: true},
-    email: {type: String, required: true},
-    birthday: Date,
-    favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-  });
-  
-  let Movie = mongoose.model('Movie', movieSchema);
-  let User = mongoose.model('User', userSchema);
-  
-  module.exports.Movie = Movie;
-  module.exports.User = User;
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  genre: {
+    name: String,
+    description: String
+  },
+  director: {
+    name: String,
+    bio: String
+  }
+});
+
+let userSchema = mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  birthday: Date,
+  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.Password);
+};
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
