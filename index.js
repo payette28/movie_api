bb9bcb8da02793d502b894e8a3a4beeb53f6e8c7
payette28 <133899413+payette28@users.noreply.github.com>
2023-08-18 23:08:20 -0400

    Update index.js
    if applied more secure connection uri

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Models = require('./models.js');
-mongoose.connect('mongodb://127.0.0.1:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://127.0.0.1:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const Movies = Models.Movie;
 const Users = Models.User;
 
