8181a589b2cc8ca945d8aba010c6cd19962c0a10
payette28 <133899413+payette28@users.noreply.github.com>
2023-09-13 15:32:39 -0400

    Update passport.js
    revert to last functioning update

--- passport.js
+++ passport.js
@@ -23,34 +23,29 @@ passport.use(
                             message: 'Incorrect username or password',
                         });
                     }
-                    if (!user.validatePassword(password)) {
-                        console.log('incorrect password');
-                        return callback(null, false, { message: 'Incorrect password.' });
-                    }
                     console.log('finished');
                     return callback(null, user);
                 })
                 .catch((error) => {
-                    if (error) {
-                        console.log(error);
-                        return callback(error);
+                if (error) {
+                    console.log(error);
+                    return callback(error);
 
-                    }
-                })
         }
-    )
+      })
+    }
+  )
 );
-
 
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
     secretOrKey: 'your_jwt_secret'
-}, async (jwtPayload, callback) => {
+}, async (jwtPayload,callback) => {
     return await Users.findById(jwtPayload._id)
-        .then((user) => {
-            return callback(null, user);
-        })
-        .catch((error) => {
-            return callback(error)
-        });
+    .then((user) => {
+        return callback(null, user);
+    })
+    .catch((error) => {
+        return callback(error)
+    });
 }));
